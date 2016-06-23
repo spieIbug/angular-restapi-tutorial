882d4d0b9855d0d5cf1b2aa5a045ce9112dc8973
Yacine <my.meddah@gmail.com>
2016-06-23 16:19:11 +0200

    databinding ok

--- webapp/js/controllers/fournisseurs.js
+++ webapp/js/controllers/fournisseurs.js
@@ -7,4 +7,27 @@ angular.module('FournisseursModule').controller('FournisseursCtrl', function($sc
         nom : 'Yacine',
         adresse : 'Lyon, France'
     };
+    $scope.fournisseurs =
+    [
+        {
+            id:1,
+            nom : 'Yacine',
+            adresse : 'Lyon, France'
+        }
+        ,{
+            id:2,
+            nom : 'MEDDAH',
+            adresse : 'Lyon, France'
+        }
+        ,{
+            id:3,
+            nom : 'John',
+            adresse : 'Saida'
+        }
+        ,{
+            id:4,
+            nom : 'Doe',
+            adresse : 'Algérie'
+        }
+    ];
 });
--- webapp/js/controllers/produits.js
+++ webapp/js/controllers/produits.js
@@ -12,5 +12,31 @@
             prixTtc : '0.00',
             fournisseur : null
         };
+        $scope.produits = [
+            {
+                id:'1',
+                libelle : 'Formation angular',
+                prixHt : '0.00',
+                tva : '5',
+                prixTtc : '0.00',
+                fournisseur : null
+            },
+            {
+                id:'2',
+                libelle : 'Modern webapp design',
+                prixHt : '0.00',
+                tva : '5',
+                prixTtc : '0.00',
+                fournisseur : null
+            },
+            {
+                id:'3',
+                libelle : 'REST API, SPA, RIA',
+                prixHt : '0.00',
+                tva : '5',
+                prixTtc : '0.00',
+                fournisseur : null
+            }
+        ];
     });
 })();
